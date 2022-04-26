c7840fb0809e2e94ef1281e373dfb161a9f21c98
daku-souru <Adrian.Ruppert@Student.HTW-Berlin.de>
2022-04-26 14:54:53 +0200

    KP-84 Customize Header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,147 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh Lpr lff">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+      <q-toolbar class="shadow-2 rounded-borders">
+        <q-toolbar-title class="text-center"
+          ><q-avatar size="65px" class="q-ma-xs q-mr-md">
+            <img src="/favicon.ico" />
+          </q-avatar>
+          Tanuki Hardware Shop
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn
+          class="bg-white text-dark q-mr-sm"
+          round
+          dense
+          :icon="$q.dark.isActive ? 'sunny' : 'nights_stay'"
+          @click="toggleDarkMode"
+        />
+
+        <q-btn-dropdown
+          class="bg-white text-black q-ma-md"
+          text-primary
+          v-bind:label="selectedCurrency.name"
+        >
+          <q-list>
+            <q-item
+              v-for="currency in currencies"
+              :key="currency.name"
+              v-bind="currency"
+              clickable
+              v-close-popup
+              @click="selectCurrency(currency)"
+            >
+              <q-item-section>{{ currency.name }}</q-item-section>
+              <q-item-section side>{{ currency.symbol }}</q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
+  <router-view name="dialog" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
     title: 'Docs',
     caption: 'quasar.dev',
     icon: 'school',
-    link: 'https://quasar.dev'
+    link: 'https://quasar.dev',
   },
   {
     title: 'Github',
     caption: 'github.com/quasarframework',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
+    link: 'https://github.com/quasarframework',
   },
   {
     title: 'Discord Chat Channel',
     caption: 'chat.quasar.dev',
     icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    link: 'https://chat.quasar.dev',
   },
   {
     title: 'Forum',
     caption: 'forum.quasar.dev',
     icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    link: 'https://forum.quasar.dev',
   },
   {
     title: 'Twitter',
     caption: '@quasarframework',
     icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    link: 'https://twitter.quasar.dev',
   },
   {
     title: 'Facebook',
     caption: '@QuasarFramework',
     icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    link: 'https://facebook.quasar.dev',
   },
   {
     title: 'Quasar Awesome',
     caption: 'Community Quasar projects',
     icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+    link: 'https://awesome.quasar.dev',
+  },
+];
+
+// Mock list
+const currencyList = [
+  {
+    name: 'EUR',
+    symbol: '€',
+  },
+  {
+    name: 'USD',
+    symbol: '$',
+  },
+  {
+    name: 'YEN',
+    symbol: '¥',
+  },
 ];
 
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    EssentialLink
+  components: {},
+
+  methods: {
+    selectCurrency(currency: { name: string; symbol: string }) {
+      this.selectedCurrency = currency;
+      console.log('Currency set to ' + currency.name);
+    },
+
+    logout() {
+      //void router.push(/); ?
+      //keycloak.logout;
+      console.log('Logged out');
+    },
+
+    toggleDarkMode() {
+      // $q.dark.isActive();
+      this.$q.dark.toggle();
+    },
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
+      currencies: currencyList,
+      selectedCurrency: ref({
+        name: 'EUR',
+        symbol: '€',
+      }),
+    };
+  },
 });
 </script>
